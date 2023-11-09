a6cfdd03ff439e5ea61bd95e9946e3f0be0742ab
Natnipa S <s.natnipa@gmail.com>
2023-11-09 14:36:35 +0100

    add WidgetsFlutterBinding

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
@@ -19,7 +20,7 @@ void main() async {
   // Load the user's preferred theme while the splash screen is displayed.
   // This prevents a sudden theme change when the app is first displayed.
   await settingsController.loadSettings();
-  // await _configureAmplify();
+  await _configureAmplify();
 
   // Run the app and pass in the SettingsController. The app listens to the
   // SettingsController for changes, then passes it further down to the
